83c7161648ba2720a9580402e2768d6f70b28658
github username <github email address>
2019-07-04 18:11:04 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,3 +17,30 @@ def swap_elements(array)
     array[1], array[2]=array[2], array[1]
     return array
 end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each {|item| item[2]='$'}
+    return array
+end
+
+def find_a(array)
+    array.select{|x| x[0]=='a'}
+end
+
+def sum_array(array)
+    array.reduce {|acc, tic| acc+tic}
+end
+
+def add_s(array)
+    array.each_with_index.map do |tic, index|
+        if index!=1
+            tic=tic+'s'
+        else
+            tic
+        end
+    end
+end
